82ea2b5ff7567e3e25da57d313f559cf08ac250c
thedylone <thedylone@gmail.com>
2022-02-27 03:52:33 +0800

    move background back and add waitlist

--- cogs/slash/game.py
+++ cogs/slash/game.py
@@ -21,7 +21,8 @@ else:
 class Game(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        # self.valorantcycle.start()
+        self.waitlist = {}
+        self.valorant_watch_cycle.start()
 
     @commands.slash_command(name='jewels',
                             description='ping jewels role',
@@ -34,7 +35,7 @@ class Game(commands.Cog):
     @commands.slash_command(name='valorant-info',
                             description='view valorant data in database',
                             guild_ids=config['guilds'])
-    async def valorantinfo(self, inter: disnake.ApplicationCommandInteraction):
+    async def valorant_info(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.defer()
         """returns user's valorant info from the database"""
         playerData = json_helper.load()
@@ -69,7 +70,7 @@ class Game(commands.Cog):
     @commands.slash_command(name='valorant-watch',
                             description='adds user into database',
                             guild_ids=config['guilds'])
-    async def valorantwatch(self, inter: disnake.ApplicationCommandInteraction, name: str, tag: str):
+    async def valorant_watch(self, inter: disnake.ApplicationCommandInteraction, name: str, tag: str):
         await inter.response.defer()
         """add user's valorant info to the database"""
         playerData = json_helper.load()
@@ -92,9 +93,9 @@ class Game(commands.Cog):
                     await inter.edit_original_message(content=f"<@{user_id}> error connecting, database not updated. please try again")
 
     @commands.slash_command(name='valorant-unwatch',
-                            description='removes user from database',
+                            description="removes user's valorant info from the database",
                             guild_ids=config['guilds'])
-    async def valorantunwatch(self, inter: disnake.ApplicationCommandInteraction):
+    async def valorant_unwatch(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.defer()
         """removes user's valorant info from the database"""
         playerData = json_helper.load()
@@ -106,6 +107,92 @@ class Game(commands.Cog):
         else:
             await inter.edit_original_message(content=f"<@{user_id}> error updating, user not in database")
 
+    @commands.slash_command(name='valorant-wait',
+                            description='pings you when tagged user is done',
+                            guild_ids=config['guilds'])
+    async def valorant_wait(self, inter: disnake.ApplicationCommandInteraction, wait_user: disnake.User):
+        await inter.response.defer()
+        """pings you when tagged user is done"""
+        playerData = json_helper.load()
+        wait_user_id = str(wait_user.id)
+        inter_user_id = str(inter.user.id)
+        extra_message = ''
+        if wait_user_id == inter_user_id:
+            extra_message = 'interesting but ok. '
+        if wait_user_id in playerData:
+            if wait_user_id in self.waitlist:
+                if inter_user_id in self.waitlist[wait_user_id]:
+                    await inter.edit_original_message(content=f"{extra_message}<@{inter_user_id}> you are already waiting for <@{wait_user_id}>")
+                    return
+                else:
+                    self.waitlist[wait_user_id] += [inter_user_id]
+            else:
+                self.waitlist[wait_user_id] = [inter_user_id]
+            await inter.edit_original_message(content=f"{extra_message}<@{inter_user_id}> success, will notify when <@{wait_user_id}> is done")
+        else:
+            await inter.edit_original_message(content=f"{extra_message}<@{wait_user_id}> is not in database, unable to execute")
+    
+    @commands.slash_command(name='valorant-waitlist',
+                            description='prints valorant waitlist',
+                            guild_ids=config['guilds'])
+    async def valorant_waitlist(self, inter: disnake.ApplicationCommandInteraction):
+        await inter.response.defer()
+        """prints valorant waitlist"""
+        embed = disnake.Embed(
+            title="valorant waitlist",
+            description="waitlist of watched users"
+        )
+        for user_id in self.waitlist:
+            embed.add_field(
+                name="user",
+                value=f"<@{user_id}>",
+                inline=False
+            )
+            embed.add_field(
+                name="waiters",
+                value=f"<@{'> <@'.join(self.waitlist[user_id])}>"
+            )
+        await inter.edit_original_message(embed=embed)
+
+    @tasks.loop(seconds=30)
+    async def valorant_watch_cycle(self):
+        await self.bot.wait_until_ready()  # wait until the bot logs in
+        channel = self.bot.get_channel(config['watch_channel']) # retrieves channel ID from config.json
+        playerData = json_helper.load()
+        for user_id in playerData:
+            playerData = json_helper.load() # reloads playerData 
+            user_data = playerData[user_id]
+            if time.time() - user_data['lastTime'] > config["watch_cooldown"] * 60: # cooldown in seconds
+                puuid = user_data['puuid']
+                region = user_data['region']
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f'https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/{region}/{puuid}') as request:
+                        # using this until access for riot granted async with session.get(f'https://{region}.api.riotgames.com/val/match/v1/matchlists/by-puuid/{puuid}?api_key={RIOT_TOKEN}') as request:
+                        if request.status == 200:
+                            data = await request.json()
+                            if len(data['data']):
+                                latestGame = data['data'][0]
+                                startTime = latestGame['metadata']['game_start'] # given in s
+                                duration = latestGame['metadata']['game_length'] / 1000 # given in ms 
+                                recentTime = startTime + duration + 100
+                                if user_data['lastTime'] < recentTime: # if latest game played is more recent than stored latest
+                                    party = [new_user_id for player in latestGame['players']['all_players'] for new_user_id in playerData if player['puuid'] == playerData[new_user_id]['puuid']]
+                                    # detects if multiple watched users are in the same game
+                                    embed = disnake.Embed(
+                                        title="valorant watch",
+                                        description=f"<@{'> and <@'.join(party)}> just finished a game at <t:{int(recentTime)}>!"
+                                    )
+                                    await channel.send(embed=embed) # sends the notification embed
+
+                                    combined_waiters = [] # init list of users to ping for waitlist
+                                    for member_id in party:
+                                        # sets party members to update last updated time if more recent
+                                        if playerData[member_id]['lastTime'] < recentTime: playerData[member_id]['lastTime'] = recentTime
+                                        if member_id in self.waitlist:
+                                            combined_waiters += self.waitlist.pop(member_id)
+                                    if combined_waiters: await channel.send(f"<@{'> <@'.join(combined_waiters)}> removing from waitlist") # pings waiters
+                                    json_helper.save(playerData)
+            await asyncio.sleep(0.5) # sleeps for number of seconds (avoid rate limit)
                           
 
 def setup(bot: commands.Bot):
