e4d6366e554ca659a30071edcbd43eb36df376b5
thedylone <thedylone@gmail.com>
2022-03-11 14:03:26 +0800

    cache data for background

--- cogs/background/background.py
+++ cogs/background/background.py
@@ -3,7 +3,6 @@ from disnake.ext import tasks, commands
 
 import aiohttp
 import asyncio
-import time
 import math
 import random
 
@@ -21,19 +20,16 @@ class Background(commands.Cog):
     @tasks.loop(seconds=30)
     async def valorant_watch_cycle(self):
         await self.bot.wait_until_ready()  # wait until the bot logs in
-        player_data = json_helper.load("playerData.json")
-        init_list = [key for key in player_data.keys()]
+        init_list = [key for key in self.bot.player_data.keys()]
         for user_id in init_list:
-            player_user = await self.bot.getch_user(int(user_id)) # reloads player_data
-            player_data = json_helper.load("playerData.json")
+            player_user = await self.bot.getch_user(int(user_id))
             if (
-                player_user == None or user_id not in player_data
+                player_user == None or user_id not in self.bot.player_data
             ):  # player no longer exists
-                # del player_data[user_id]
-                # json_helper.save(player_data, "playerData.json")
+                # del self.bot.player_data[user_id]
+                # json_helper.save(self.bot.player_data, "playerData.json")
                 continue
-            guild_data = json_helper.load("guildData.json")
-            user_data = player_data[user_id]
+            user_data = self.bot.player_data[user_id]
             user_puuid = user_data["puuid"]
             user_region = user_data["region"]
             user_guild = user_data["guild"]
@@ -43,7 +39,7 @@ class Background(commands.Cog):
             guild_exists = self.bot.get_guild(user_guild)
             if guild_exists in self.bot.guilds:
                 # check if bot is still in the guild
-                watch_channel_id = guild_data[str(user_guild)]["watch_channel"]
+                watch_channel_id = self.bot.guild_data[str(user_guild)]["watch_channel"]
                 channel_exists = self.bot.get_channel(watch_channel_id)
                 guild_exists_channels = guild_exists.text_channels
                 if channel_exists in guild_exists_channels:
@@ -52,8 +48,8 @@ class Background(commands.Cog):
                     channel_safe = True
                 elif watch_channel_id:
                     # sends a warning that guild exists but channel is gone
-                    guild_data[str(user_guild)]["watch_channel"] = 0
-                    json_helper.save(guild_data, "guildData.json")
+                    self.bot.guild_data[str(user_guild)]["watch_channel"] = 0
+                    json_helper.save(self.bot.guild_data, "guildData.json")
                     if guild_exists_channels:
                         await guild_exists_channels[0].send(
                             "The channel I am set to no longer exists! Please use valorant-setchannel on another channel. I will send updates to members directly instead."
@@ -61,7 +57,7 @@ class Background(commands.Cog):
             elif user_guild:
                 # sends a DM to the user that bot is no longer in the guild
                 user_data["guild"] = 0
-                json_helper.save(player_data, "playerData.json")
+                json_helper.save(self.bot.player_data, "playerData.json")
                 await player_user.send(
                     "I am no longer in the server you initialised valorant-watch in. I will send updates to you directly. Alternatively, you can /valorant-unwatch to stop receiving updates, or perform /valorant-watch in another server."
                 )
@@ -98,13 +94,15 @@ class Background(commands.Cog):
                         rounds_red += round["winning_team"] == "Red"
                         rounds_blue += round["winning_team"] == "Blue"
                     for player in latest_game["players"]["all_players"]:
-                        for player_id in player_data:
+                        for player_id in self.bot.player_data:
                             if (
                                 player_id == user_id
                                 and player["puuid"] == user_puuid
                                 or user_guild > 0
-                                and player["puuid"] == player_data[player_id]["puuid"]
-                                and player_data[player_id]["guild"] == user_guild
+                                and player["puuid"]
+                                == self.bot.player_data[player_id]["puuid"]
+                                and self.bot.player_data[player_id]["guild"]
+                                == user_guild
                             ):  # detects if multiple watched users who "watched" in the same guild (not 0) are in the same game
                                 player_stats = player["stats"]
                                 player_acs = player_stats["score"] / rounds_played
@@ -114,7 +112,7 @@ class Background(commands.Cog):
                                 elif team == "Blue":
                                     party_blue.append(player_id)
                                 elif (
-                                    team == player_data[player_id]["puuid"]
+                                    team == self.bot.player_data[player_id]["puuid"]
                                 ):  # deathmatch exception
                                     party_red.append(player_id)
 
@@ -138,21 +136,35 @@ class Background(commands.Cog):
 
                                 if mode == "Competitive" or mode == "Unrated":
                                     # save stats
-                                    player_data[player_id]["headshots"] = player_data[
-                                        player_id
-                                    ]["headshots"][-4:] + [player_stats["headshots"]]
+                                    self.bot.player_data[player_id][
+                                        "headshots"
+                                    ] = self.bot.player_data[player_id]["headshots"][
+                                        -4:
+                                    ] + [
+                                        player_stats["headshots"]
+                                    ]
 
-                                    player_data[player_id]["bodyshots"] = player_data[
-                                        player_id
-                                    ]["bodyshots"][-4:] + [player_stats["bodyshots"]]
+                                    self.bot.player_data[player_id][
+                                        "bodyshots"
+                                    ] = self.bot.player_data[player_id]["bodyshots"][
+                                        -4:
+                                    ] + [
+                                        player_stats["bodyshots"]
+                                    ]
 
-                                    player_data[player_id]["legshots"] = player_data[
-                                        player_id
-                                    ]["legshots"][-4:] + [player_stats["legshots"]]
+                                    self.bot.player_data[player_id][
+                                        "legshots"
+                                    ] = self.bot.player_data[player_id]["legshots"][
+                                        -4:
+                                    ] + [
+                                        player_stats["legshots"]
+                                    ]
 
-                                    player_data[player_id]["acs"] = player_data[
-                                        player_id
-                                    ]["acs"][-4:] + [player_acs]
+                                    self.bot.player_data[player_id][
+                                        "acs"
+                                    ] = self.bot.player_data[player_id]["acs"][-4:] + [
+                                        player_acs
+                                    ]
 
                     async with session.get(
                         "https://api.henrikdev.xyz/valorant/v1/content"
@@ -189,19 +201,24 @@ class Background(commands.Cog):
                         feeder_images = [
                             "https://intinc.com/wp-content/uploads/2022/02/INT-Logo-Update_2104.png"
                         ]
-                        if user_guild != 0 and str(user_guild) in guild_data:
+                        if user_guild != 0 and str(user_guild) in self.bot.guild_data:
                             if (
-                                "feeder_messages" in guild_data[str(user_guild)]
-                                and guild_data[str(user_guild)]["feeder_messages"]
+                                "feeder_messages"
+                                in self.bot.guild_data[str(user_guild)]
+                                and self.bot.guild_data[str(user_guild)][
+                                    "feeder_messages"
+                                ]
                             ):
-                                feeder_messages = guild_data[str(user_guild)][
+                                feeder_messages = self.bot.guild_data[str(user_guild)][
                                     "feeder_messages"
                                 ]
                             if (
-                                "feeder_images" in guild_data[str(user_guild)]
-                                and guild_data[str(user_guild)]["feeder_images"]
+                                "feeder_images" in self.bot.guild_data[str(user_guild)]
+                                and self.bot.guild_data[str(user_guild)][
+                                    "feeder_images"
+                                ]
                             ):
-                                feeder_images = guild_data[str(user_guild)][
+                                feeder_images = self.bot.guild_data[str(user_guild)][
                                     "feeder_images"
                                 ]
 
@@ -232,20 +249,24 @@ class Background(commands.Cog):
                         # streak function
                         if rounds_red != rounds_blue:
                             if rounds_red > rounds_blue:
-                                new_streak = max(player_data[member_id]["streak"] + 1, 1)
+                                new_streak = max(
+                                    self.bot.player_data[member_id]["streak"] + 1, 1
+                                )
                             elif rounds_red < rounds_blue:
-                                new_streak = min(player_data[member_id]["streak"] - 1, -1)
+                                new_streak = min(
+                                    self.bot.player_data[member_id]["streak"] - 1, -1
+                                )
                             if abs(new_streak) >= 3:
                                 is_streak = True
                                 streak_embed.add_field(
                                     name="streaker",
                                     value=f"<@{member_id}> is on a {abs(new_streak)}-game {'winning' if new_streak > 0 else 'losing'} streak!",
                                 )
-                            player_data[member_id]["streak"] = new_streak
+                            self.bot.player_data[member_id]["streak"] = new_streak
 
                         # sets party members to update last updated time if more recent
-                        player_data[member_id]["lastTime"] = max(
-                            player_data[member_id]["lastTime"], recent_time
+                        self.bot.player_data[member_id]["lastTime"] = max(
+                            self.bot.player_data[member_id]["lastTime"], recent_time
                         )
                         if member_id in self.bot.valorant_waitlist:
                             combined_waiters += self.bot.valorant_waitlist.pop(
@@ -256,9 +277,13 @@ class Background(commands.Cog):
                         # streak function
                         if rounds_red != rounds_blue:
                             if rounds_blue > rounds_red:
-                                new_streak = max(player_data[member_id]["streak"] + 1, 1)
+                                new_streak = max(
+                                    self.bot.player_data[member_id]["streak"] + 1, 1
+                                )
                             elif rounds_blue < rounds_red:
-                                new_streak = min(player_data[member_id]["streak"] - 1, -1)
+                                new_streak = min(
+                                    self.bot.player_data[member_id]["streak"] - 1, -1
+                                )
                             if abs(new_streak) >= 3:
                                 is_streak = True
                                 streak_embed.add_field(
@@ -266,11 +291,11 @@ class Background(commands.Cog):
                                     value=f"<@{member_id}> is on a {abs(new_streak)}-game {'winning' if new_streak > 0 else 'losing'} streak!",
                                     inline=False,
                                 )
-                            player_data[member_id]["streak"] = new_streak
+                            self.bot.player_data[member_id]["streak"] = new_streak
 
                         # sets party members to update last updated time if more recent
-                        player_data[member_id]["lastTime"] = max(
-                            player_data[member_id]["lastTime"], recent_time
+                        self.bot.player_data[member_id]["lastTime"] = max(
+                            self.bot.player_data[member_id]["lastTime"], recent_time
                         )
                         if member_id in self.bot.valorant_waitlist:
                             combined_waiters += self.bot.valorant_waitlist.pop(
@@ -293,7 +318,7 @@ class Background(commands.Cog):
                                         "A player you were waiting for is done!"
                                     )
 
-                    json_helper.save(player_data, "playerData.json")
+                    json_helper.save(self.bot.player_data, "playerData.json")
             await asyncio.sleep(0.5)  # sleeps for number of seconds (avoid rate limit)
 
 
