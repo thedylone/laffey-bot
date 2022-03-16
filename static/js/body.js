450a3b6a14b0799c211bb47eb09f3238ac67ca09
thedylone <thedylone@gmail.com>
2022-03-16 15:35:10 +0800

    show autocomplete dropdown

--- static/js/body.js
+++ static/js/body.js
@@ -1,23 +1,54 @@
 function autocomplete(input, arr) {
     input.on("input", function (e) {
         inputText = this.value;
+        $("#autocomplete-list").empty()
         var displayed = []
         arr.forEach(a => {
             var group = $("#" + a)
             if (a.toLowerCase().includes(inputText.toLowerCase())) {
                 group.css("display", "block");
-                displayed.push(group)
+                displayed.push(a)
             } else {
                 group.css("display", "none");
                 group.css("animation", 'none');
             }
         })
-        displayed.forEach(e => {
-            var z = $(e).offset().top, o = $(window).height(), a = $(window).scrollTop()
-            if (z - 0.8 * o <= a && a < z) {
-                window.matchMedia("(max-width: 800px)").matches ? w = '0s' : w = $(e).attr("animation-delay");
-                $(e).css("animation", $(e).attr("animation-style") + " " + $(e).attr("animation-duration") + " forwards " + w + " ease-in-out");
+        if (inputText) {
+            displayed.forEach(a => {
+                $("#autocomplete-list")
+                    .append($("<a></a>")
+                        .attr("class", "autocomplete-item js-anchor")
+                        .attr("href", "#" + a)
+                        .html(a)
+                        .on("click", function () { var e; if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname && (e = (e = $(this.hash)).length ? e : $("[name=" + this.hash.slice(1) + "]")).length) return $("html,body").animate({ scrollTop: e.offset().top - $('.first-nav').height() - $(".js-nav").show().height() }, 1000), !1 })
+                    )
+            })
+            $("#autocomplete-list").css("display", "inline-block");
+            if (displayed.length == 0) {
+                $("#autocomplete-list").append($("<a></a>")
+                    .attr("class", "autocomplete-item")
+                    .html("no results found")
+                )
             }
-        })
+        } else {
+            $("#autocomplete-list").hide()
+        }
+        scrollanimate();
     })
+}
+
+function searchAnchor() {
+    $(".search-anchor").on("click", function (e) {
+        $($(this).attr('href')).show()
+    })
+}
+
+function scrollanimate() {
+    $(".animate__animated").each(function () {
+        z = $(this).offset().top, o = $(window).height(), y = $(this).height(), t = $(window).scrollTop();
+        if (z - 0.8 * o <= t && t < z + y) {
+            window.matchMedia("(max-width: 800px)").matches ? w = '0s' : w = $(this).attr("animation-delay");
+            $(this).css("animation", $(this).attr("animation-style") + " " + $(this).attr("animation-duration") + " forwards " + w + " ease-in-out");
+        }
+    });
 }
